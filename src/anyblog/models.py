b428bc5a27a9d70cad80cae67c5e6f011df8138c
Jon Dunleavy <admin@defmyfunc.com>
2011-12-02 22:49:46 +0000

    added BlogPost model

--- src/anyblog/models.py
+++ src/anyblog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.CharField(max_length=50)
+    timestampcreated = models.DateTimeField(auto_now_add=True)
+    timestampmodified = models.DateTimeField(auto_now=True)
