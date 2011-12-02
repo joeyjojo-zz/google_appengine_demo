c9dd2db196dc6bf77a0842b31c9bfe0542d9710a
Jon Dunleavy <admin@defmyfunc.com>
2011-12-02 23:19:31 +0000

    added unicode method to BlogPost model

--- src/anyblog/models.py
+++ src/anyblog/models.py
@@ -7,3 +7,12 @@ class BlogPost(models.Model):
     author = models.CharField(max_length=50)
     timestampcreated = models.DateTimeField(auto_now_add=True)
     timestampmodified = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return u"<BlogPost (title={0}, content={1}, author={2}, created={3}, modified={4})>".format(
+            self.title,
+            self.content,
+            self.author,
+            self.timestampcreated,
+            self.timestampmodified
+        )
