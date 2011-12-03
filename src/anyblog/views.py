0953aaa20d9dcaea0a19f2f8fd5df318149bf537
Jon Dunleavy <admin@defmyfunc.com>
2011-12-03 00:01:38 +0000

    simple retrieval of blog data working

--- src/anyblog/views.py
+++ src/anyblog/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+import models
+
+def index(request):
+    latest_blog_list = models.BlogPost.objects.all().order_by('-timestampcreated')[:5]
+    return render_to_response('anyblog/index.html', {'latest_blog_list': latest_blog_list})
