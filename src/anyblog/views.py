7a9cefa9eb40e7cd5b413432137b87aace651598
Jon Dunleavy <admin@defmyfunc.com>
2011-12-05 19:04:06 +0000

    added some comments

--- src/anyblog/views.py
+++ src/anyblog/views.py
@@ -10,6 +10,9 @@ import models
 logger = logging.getLogger(__name__)
 
 def index(request):
+    """
+    Returns the template for the main page.
+    """
     # get the blogs we need
     # TODO: Cut this down to the first 100 chars of post
     latest_blog_list = models.BlogPost.objects.all().order_by('-timestampcreated')[:5]
@@ -27,9 +30,21 @@ def index(request):
                                              'year_month_dict': d})
 
 def detail(request, blogpost_id):
+    """
+    Returns the template for an idividual blog post
+    @param blogpost_id: The id of the blog post to display
+    @type blogpost_id: int
+    """
     # TODO: make this right
     return render_to_response('index.html')
 
 def month_archive(request, year, month):
+    """
+    Returns an archive view for all the blogposts against that year/month
+    @param year: The year of the blog posts to display
+    @type year: int
+    @param month: The month of the blog posts to display
+    @type month: int
+    """
     # TODO: make this right
     return render_to_response('index.html')
